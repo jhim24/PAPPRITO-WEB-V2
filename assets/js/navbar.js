5280fde1358556274ce2acdcc104e3cb6c47fff0
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-18 20:46:28 +0300

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,1 +1,44 @@
+/* ==========================================================
+   PAPPRITO WEB V2
+   NAVBAR MODULE
+   File : assets/js/components/navbar.js
+   Version : 0.0.1
+   Description : Mobile Navigation Functions
+========================================================== */
 
+
+/* ==========================================================
+   SELECT ELEMENTS
+========================================================== */
+
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navMenu = document.querySelector(".nav-menu");
+
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+
+/* ==========================================================
+   OPEN / CLOSE MENU
+========================================================== */
+
+menuToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
+});
+
+
+/* ==========================================================
+   AUTO CLOSE MENU
+========================================================== */
+
+navLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navMenu.classList.remove("active");
+
+    });
+
+});
