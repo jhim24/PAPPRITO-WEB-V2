451431cf5ac53ef1a02efc602c8ec94c2d033458
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-19 09:04:20 +0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,40 +1,20 @@
 /* ==========================================================
    PAPPRITO WEB V2
-   Version : 1.5.2
    File : assets/js/firebase-config.js
-   Description : Firebase Configuration
-========================================================== */
-
-/* ==========================================================
-   FIREBASE CONFIG
 ========================================================== */
 
 const firebaseConfig = {
-
-    apiKey: "",
-
-    authDomain: "",
-
-    databaseURL: "",
-
-    projectId: "",
-
-    storageBucket: "",
-
-    messagingSenderId: "",
-
-    appId: ""
-
+    apiKey: "AIzaSyAV9T5w_1azmPHIJcZpraXP06TItj7HEuA",
+    authDomain: "papprito-orders.firebaseapp.com",
+    databaseURL: "https://papprito-orders-default-rtdb.firebaseio.com",
+    projectId: "papprito-orders",
+    storageBucket: "papprito-orders.firebasestorage.app",
+    messagingSenderId: "831941801424",
+    appId: "1:831941801424:web:40a99cdfb312dac2d275d5"
 };
-
-/* ==========================================================
-   INITIALIZE FIREBASE
-========================================================== */
 
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
-
 const auth = firebase.auth();
-
 const storage = firebase.storage();
