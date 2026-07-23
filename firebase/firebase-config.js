fd78f01c39f73d28a28c83d1921c6f8b9d214f84
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-23 23:30:21 +0300

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,1 +1,36 @@
+// ==========================================
+// PAPPRITO ERP
+// FIREBASE CONFIG
+// ==========================================
 
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAV9T5w_1azmPHIJcZpraXP06TItj7HEuA",
+
+    authDomain: "papprito-orders.firebaseapp.com",
+
+    databaseURL: "https://papprito-orders-default-rtdb.firebaseio.com",
+
+    projectId: "papprito-orders",
+
+    storageBucket: "papprito-orders.firebasestorage.app",
+
+    messagingSenderId: "831941801424",
+
+    appId: "1:831941801424:web:40a99cdfb312dac2d275d5"
+
+};
+
+// Initialize once only
+
+if(!firebase.apps.length){
+
+    firebase.initializeApp(firebaseConfig);
+
+}
+
+// Global Database
+
+const db = firebase.database();
+
+console.log("✅ PAPPRITO ERP Firebase Connected");
